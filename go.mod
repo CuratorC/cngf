036ec231362b4614ea958cb03a04a80b6db30d35
CuratorC <mail@curatorc.com>
2022-01-21 09:25:15 +0800

    迁移文件

---
+++ go.mod
@@ -0,0 +1,62 @@
+module github.com/curatorc/cngf
+
+go 1.17
+
+require (
+	github.com/KenmyZhang/aliyun-communicate v0.0.0-20180308134849-7997edc57454 // 阿里云短信服务
+	github.com/disintegration/imaging v1.6.2 // 图片裁切
+	github.com/gertd/go-pluralize v0.1.7 // 单词单复数转换
+	github.com/gin-gonic/gin v1.7.7 // Go 路由工具
+	github.com/go-redis/redis/v8 v8.11.4 // redis 存储
+	github.com/golang-jwt/jwt v3.2.2+incompatible // JWT
+	github.com/iancoleman/strcase v0.2.0 // 驼峰格式转换
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // 邮箱服务
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // 控制台高亮
+	github.com/mojocn/base64Captcha v1.3.5 // 图片验证码
+	github.com/spf13/cast v1.4.1 // 数据类型转换
+	github.com/spf13/viper v1.10.1 // config & env 工具
+	github.com/ulule/limiter/v3 v3.9.0 // 限流插件
+	go.uber.org/zap v1.20.0 // 日志服务
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // Hash
+	gorm.io/gorm v1.22.4 // GORM
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/afero v1.8.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ redis/redis.go
@@ -0,0 +1,138 @@
+// Package redis 工具包
+package redis
+
+import (
+	"context"
+	"github.com/curatorc/cngf/logger"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Client Redis 服务
+type Client struct {
+	Client  *redis.Client
+	Context context.Context
+}
+
+// once 确保全局的 Redis 对象只实例一次
+var once sync.Once
+
+// Redis 全局 Redis, 使用 db 1
+var Redis *Client
+
+// Connect 连接 redis 数据库，设置全局的 Redis 对象
+func Connect(address string, username string, password string, db int) {
+	once.Do(func() {
+		Redis = New(address, username, password, db)
+	})
+}
+
+// New 创建一个新的 redis 连接
+func New(address string, username string, password string, db int) *Client {
+	// 初始化自定义的 Client 实例
+	rds := &Client{
+		Context: context.Background(),
+		Client: redis.NewClient(&redis.Options{
+			Addr:     address,
+			Username: username,
+			Password: password,
+			DB:       db,
+		}),
+	}
+
+	// 测试一下连接
+	err := rds.Ping()
+	logger.LogIf(err)
+
+	return rds
+}
+
+// Ping 用以测试 redis 连接是否正常
+func (rds Client) Ping() error {
+	_, err := rds.Client.Ping(rds.Context).Result()
+	return err
+}
+
+// Set 存储 key 对应的 value, 且设置 expiration 过期时间
+func (rds Client) Set(key string, value interface{}, expiration time.Duration) bool {
+	if err := rds.Client.Set(rds.Context, key, value, expiration).Err(); err != nil {
+		logger.ErrorString("Redis", "set", err.Error())
+		return false
+	}
+	return true
+}
+
+// Get 获取 key 对应的 value
+func (rds Client) Get(key string) string {
+	result, err := rds.Client.Get(rds.Context, key).Result()
+	if err != nil {
+		if err != redis.Nil {
+			logger.ErrorString("Redis", "Get", err.Error())
+		}
+		return ""
+	}
+	return result
+}
+
+// Has 判断一个 key 是否存在，内部错误和 redis.Nil 都返回 false
+func (rds Client) Has(key string) bool {
+	_, err := rds.Client.Get(rds.Context, key).Result()
+	if err != nil {
+		if err != redis.Nil {
+			logger.ErrorString("Redis", "Get", err.Error())
+		}
+		return false
+	}
+	return true
+}
+
+// Del 删除存储在 redis 里的数据，支持多个 key 传参
+func (rds Client) Del(keys ...string) bool {
+	if err := rds.Client.Del(rds.Context, keys...).Err(); err != nil {
+		logger.ErrorString("Redis", "Del", err.Error())
+		return false
+	}
+	return true
+}
+
+// FlushDB 清空当前 redis db 里的所有数据
+func (rds Client) FlushDB() bool {
+	if err := rds.Client.FlushDB(rds.Context).Err(); err != nil {
+		logger.ErrorString("Redis", "FlushDB", err.Error())
+		return false
+	}
+	return true
+}
+
+func (rds Client) Increment(key string) bool {
+	if err := rds.Client.Incr(rds.Context, key).Err(); err != nil {
+		logger.ErrorString("Redis", "Increment", err.Error())
+		return false
+	}
+	return true
+}
+
+func (rds Client) Increments(key string, count int64) bool {
+	if err := rds.Client.IncrBy(rds.Context, key, count).Err(); err != nil {
+		logger.ErrorString("Redis", "Increment", err.Error())
+	}
+	return true
+}
+
+func (rds Client) Decrement(key string) bool {
+	if err := rds.Client.Decr(rds.Context, key).Err(); err != nil {
+		logger.ErrorString("Redis", "Decrement", err.Error())
+		return false
+	}
+	return true
+}
+
+func (rds Client) Decrements(key string, count int64) bool {
+	if err := rds.Client.DecrBy(rds.Context, key, count).Err(); err != nil {
+		logger.ErrorString("Redis", "Decrement", err.Error())
+		return false
+	}
+	return true
+}
